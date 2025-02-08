1b7bf9b980a0c654fa3483d0de2c97f5bb6a0217
jou <maikerubahha@yahoo.co.jp>
2025-02-08 16:37:26 +0900

    カレンダー表示処理の実装

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,3 +5,14 @@ require 'optparse'
 params = ARGV.getopts("y:m:")
 year = params["y"] ? params["y"].to_i : Date.today.year
 month = params["m"] ? params["m"].to_i : Date.today.month
+
+# カレンダー表示
+print "      #{month}月 #{year}\n"
+print "日 月 火 水 木 金 土\n"
+print "   " * Date.new(year, month, 1).wday
+start_date = Date.new(year, month, 1)
+last_date = Date.new(year, month, -1)
+(start_date..last_date).each do |date|
+  print date.mday.to_s.rjust(2) + " "
+  puts "\n" if date.saturday?
+end
