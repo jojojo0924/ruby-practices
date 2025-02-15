718a2ce9d42fb74763654d844ecb89ba319e3c5b
jou <maikerubahha@yahoo.co.jp>
2025-02-15 09:23:42 +0900

    if式を使って余計な重複をなくす

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -3,11 +3,11 @@
 
 def calculate_extra_strike_points(frames, index)
   next_frame_first = frames[index + 1][0]
-  if next_frame_first == 10 # 次のフレームもストライクだった時
-    next_frame_first + frames[index + 2][0]
-  else
-    next_frame_first + frames[index + 1][1]
-  end
+  next_frame_first + if next_frame_first == 10 # 次のフレームもストライクだった時
+                       frames[index + 2][0]
+                     else
+                       frames[index + 1][1]
+                     end
 end
 
 score = ARGV[0]
@@ -27,12 +27,12 @@ frames = shots.each_slice(2).to_a
 partial_frames = frames[...9]
 total = partial_frames.each_with_index.sum(frames[9..].flatten.sum) do |frame, index|
   frame_sum = frame.sum
-  if frame[0] == 10 # ストライク
-    frame_sum + calculate_extra_strike_points(frames, index)
-  elsif frame_sum == 10 # スペア
-    frame_sum + frames[index + 1][0]
-  else
-    frame_sum
-  end
+  frame_sum + if frame[0] == 10 # ストライク
+                calculate_extra_strike_points(frames, index)
+              elsif frame_sum == 10 # スペア
+                frames[index + 1][0]
+              else
+                0
+              end
 end
 puts total
