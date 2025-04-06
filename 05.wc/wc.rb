723917fb1da29c15e15bfb1da57856b07ad10406
jou <maikerubahha@yahoo.co.jp>
2025-04-06 17:49:10 +0900

    wcコマンドの修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -8,42 +8,54 @@ OPTIONS = 'lwc'
 def main
   options = ARGV.getopts(OPTIONS)
   if ARGV.empty?
-    display($stdin.read, options)
+    file_stat = get_file_stat($stdin.read)
+    display(file_stat, options)
   elsif ARGV.size == 1
     file = ARGV.first
-    display(File.open(file).read, options, " #{file}")
+    file_stat = get_file_stat(File.open(file).read)
+    display(file_stat, options, file)
   else
     display_all(options)
   end
 end
 
 def display_all(options)
-  all_content = ''
+  total_file_stat = {
+    lines: 0,
+    words: 0,
+    bytes: 0
+  }
   ARGV.each do |file|
     content = File.open(file).read
-    all_content += content
-    display(content, options, " #{file}")
+    file_stat = get_file_stat(content)
+    display(file_stat, options, file)
+    total_file_stat[:lines] += file_stat[:lines]
+    total_file_stat[:words] += file_stat[:words]
+    total_file_stat[:bytes] += file_stat[:bytes]
   end
-  display(all_content, options, ' total')
+  display(total_file_stat, options, 'total')
 end
 
-def display(content, options, target = '')
-  lines = content.lines.count
+def display(file_stat, options, target = '')
+  lines = file_stat[:lines]
   lines_to_display = lines.to_s.rjust(calc_width(lines))
-  words = content.split(/[ \t\n]+/).size
+  words = file_stat[:words]
   words_to_display = words.to_s.rjust(calc_width(words))
-  bytes = content.bytesize
+  bytes = file_stat[:bytes]
   bytes_to_display = bytes.to_s.rjust(calc_width(bytes))
-  if options.values.all?(false)
-    print lines_to_display
-    print words_to_display
-    print bytes_to_display
-  else
-    print options['l'] ? lines_to_display : ''
-    print options['w'] ? words_to_display : ''
-    print options['c'] ? bytes_to_display : ''
-  end
-  puts target
+  options.transform_values!(&:!) if options.values.all?(false)
+  print lines_to_display if options['l']
+  print words_to_display if options['w']
+  print bytes_to_display if options['c']
+  puts " #{target}"
+end
+
+def get_file_stat(content)
+  {
+    lines: content.lines.count,
+    words: content.split(/[ \t\n]+/).size,
+    bytes: content.bytesize
+  }
 end
 
 def calc_width(number)
